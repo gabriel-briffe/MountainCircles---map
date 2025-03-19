f5cc42236c401e3a79688c20b8bc812878c79290
gabouls8 <10479082+gabouls8@users.noreply.github.com>
2025-03-19 04:52:27 +0100

    Update styles for navboxes and enhance service worker caching. Adjusted bottom positioning of navbox in portrait mode and refined padding for improved layout. Added navboxes.js to the cache for better resource management.

--- sw.js
+++ sw.js
@@ -63,6 +63,7 @@ const INITIAL_CACHE_URLS = [
   `${BASE_PATH}/passes.geojson`,
   `${BASE_PATH}/airspace.geojson`,
   `${BASE_PATH}/mappings.js`,
+  `${BASE_PATH}/navboxes.js`,
   `${BASE_PATH}/icons/icon-192.png`,
   'https://cdn.jsdelivr.net/npm/maplibre-gl@latest/dist/maplibre-gl.js',
   'https://cdn.jsdelivr.net/npm/maplibre-gl@latest/dist/maplibre-gl.css',
@@ -448,6 +449,8 @@ self.addEventListener('message', async (event) => {
       `${BASE_PATH}/igc.js`,
       `${BASE_PATH}/install.js`,
       `${BASE_PATH}/layerStyles.js`,
+      `${BASE_PATH}/navboxes.js`,
+      `${BASE_PATH}/location.js`,
       `${BASE_PATH}/sw.js`
     ];
     
